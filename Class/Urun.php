55c9a9802bf97882aa7b25f6bf9ede147c8862ca
Zekeriya Uysal <61068373+xZek@users.noreply.github.com>
2020-06-03 00:16:06 +0300

    Update Urun.php

--- Class/Urun.php
+++ Class/Urun.php
@@ -16,6 +16,40 @@ class Urun
              exit($e->getMessage());
          }
      }
+     public function Get_Category()
+     {
+       $db = DB();
+       $query = $db->query("SELECT * FROM icerik_category", PDO::FETCH_ASSOC);
+   if ( $query->rowCount() ){
+      foreach( $query as $row ){
+
+            echo '<option value="'.$row["id"].'">'.$row["category_ad"].'</option>';
+          }
+        }
+  }
+
+     //Kullanıcı Düzenleme Ekranındaki Kullanıcıları getirir
+
+     public function List_Content()
+     {
+          $db = DB();
+          $i = 0;
+          $CategoryID = "";
+          $query = $db->query("select * from icerikler INNER JOIN icerik_category ON icerik_category.id = icerikler.urun_categoryID ", PDO::FETCH_ASSOC);
+              if ( $query->rowCount() ){
+         foreach( $query as $row ){
+                $i +=1 ;
+
+               echo '<tr>';
+               echo '<td>'.$i.'</td>';
+               echo '<td>'.$row["urun_adi"].'</td>';
+               echo '<td>'.$row["urun_fiyati"].'</td>';
+               echo '<td>'.$row["category_ad"].'</td>';
+               echo '<td><a href="icerik_duzenle?ID='.$row["urun_id"].'"><button class="btn btn-success"><i class="fas fa-edit"> DÜZENLE</button></i></td>';
+               echo '</tr>';
+             }
+           }
+     }
 
 
 
