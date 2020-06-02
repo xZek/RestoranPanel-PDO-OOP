8628241a950869d1f6fb2b24c23a646d49b63c3b
Zekeriya Uysal <61068373+xZek@users.noreply.github.com>
2020-06-02 18:00:10 +0300

    Update User_update.php

--- POST/User_update.php
+++ POST/User_update.php
@@ -3,14 +3,14 @@ if(isset($_POST["edit"])) {
 
      $username = $_POST["username"];
      $password = $_POST["password"];
-
+     $user_id = $_POST['id'];
    if(empty($username))
    {
      $hataBTN = true;
      $message = "Kullanıcı adı boş bırakılamaz";
    }else if(empty($password)){
 
-     $result = $User->ChangeUsername($user->user_id, $user->username);
+     $result = $User->ChangeUsername($user_id,$username, $user->user_id, $user->username);
      $basariBTN_Timer = true;
      $message = "Başarıyla Kaydedildi";
      echo '<script>
@@ -22,7 +22,7 @@ if(isset($_POST["edit"])) {
  ';
    }else{
 
-          $result = $User->ChangeAll($user->user_id, $user->username);
+          $result = $User->ChangeAll($user_id,$username, $password, $user->user_id, $user->username);
           $basariBTN_Timer = true;
           $message = "Başarıyla Kaydedildi";
           echo '<script>
