dd72a9d320b628edc7c3f33f1ca8f888a2ba1a61
Zekeriya Uysal <61068373+xZek@users.noreply.github.com>
2020-06-09 19:12:44 +0300

    Add files via upload

---
+++ panel/icerik_duzenle.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../Site/header.php";
+require "../POST/Urun_IcerikDuzenle.php";
+//require "../POST/Urun_IcerikSil.php";
+$ID = $_GET["ID"];
+  require "../Views/Icerik_Duzenle.php";
+  require "../Site/footer.php";
+
+       ?>
