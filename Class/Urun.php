e4165bec9e6fe065d8a5300e18108dd1453d4971
Zekeriya Uysal <61068373+xZek@users.noreply.github.com>
2020-06-09 19:14:39 +0300

    Update Urun.php

--- Class/Urun.php
+++ Class/Urun.php
@@ -1,13 +1,16 @@
 <?php
 class Urun
 {
-  public function IcerikEkle($urunAdi, $urunFiyat,$urunKategoriID,$whoAdd)
+
+  //ICERIK EKLER
+  public function IcerikEkle($urunAdi, $urunFiyat, $new_name, $urunKategoriID,$whoAdd)
      {
          try {
              $db = DB();
-             $query = $db->prepare("INSERT INTO icerikler(urun_adi,urun_fiyati, urun_categoryID) VALUES (:urun_adi, :urun_fiyati, :urun_categoryID)");
+             $query = $db->prepare("INSERT INTO icerikler(urun_adi,urun_fiyati, urun_img1, urun_categoryID) VALUES (:urun_adi, :urun_fiyati, :urun_img1 , :urun_categoryID)");
              $query->bindParam("urun_adi", $urunAdi, PDO::PARAM_STR);
              $query->bindParam("urun_fiyati", $urunFiyat, PDO::PARAM_STR);
+             $query->bindParam("urun_img1", $new_name, PDO::PARAM_STR);
              $query->bindParam("urun_categoryID", $urunKategoriID, PDO::PARAM_STR);
              $query->execute();
              return $db->lastInsertId();
@@ -16,6 +19,38 @@ class Urun
              exit($e->getMessage());
          }
      }
+        //İçerik Düzenleme Fonksyonu (Kullanıcı Bilgileri'de getiriliyor Log tutulabilmesi için)
+     public function IcerikDuzenle($userID, $username, $icerik_ID,$icerik_adi,$icerik_fiyati,$urunKategoriID)
+      {
+          $db = DB();
+        $update = "UPDATE icerikler SET urun_adi = '" . $icerik_adi . "', urun_fiyati = '" . $icerik_fiyati . "',  urun_categoryID = '" . $urunKategoriID . "'  WHERE urun_id=" . $icerik_ID;
+        $done =  $db->query($update);
+        if($done)
+        {
+        // OTOMATIK LOG INPUTU
+          $Proccess = 'Urun Bilgisi Güncelledi - Güncellenen Ürün ID : '.$icerik_ID.' ';
+
+                    //BURADA LOG EKLIYORUZ
+                  $query = $db->prepare("INSERT INTO activity_log(userID,procces, who_didthis) VALUES (:userID, :procces, :who_didthis)");
+                  $query->bindParam("userID", $userID, PDO::PARAM_STR);
+                  $query->bindParam("procces", $Proccess, PDO::PARAM_STR);
+                  $query->bindParam("who_didthis", $username , PDO::PARAM_STR);
+                  $query->execute();
+           }
+           else{
+                echo "Sitede Bir Sorun oluştu destek ekiple iletişime geçiniz";
+                exit();
+           }
+
+     }
+
+
+
+
+
+
+
+     // KATEGORILERI LISTELER
      public function Get_Category()
      {
        $db = DB();
@@ -26,10 +61,9 @@ class Urun
             echo '<option value="'.$row["id"].'">'.$row["category_ad"].'</option>';
           }
         }
-  }
+     }
 
-     //İçerikleri Listeler
-
+     //ICERIKLERI LİSTELER
      public function List_Content()
      {
           $db = DB();
